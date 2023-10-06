cd714a1a2dcee3155504e2b1a0173c72634b43fd
Anderson Chaves <108032363+chaavez@users.noreply.github.com>
2023-10-06 19:31:24 -0300

    Created register fragment in project and added its resources

---
+++ app/src/main/java/br/com/portifolio/movieapp/features/auth/register/RegisterFragment.kt
@@ -0,0 +1,18 @@
+package br.com.portifolio.movieapp.features.auth.register
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.com.portifolio.movieapp.R
+
+class RegisterFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_register, container, false)
+    }
+}

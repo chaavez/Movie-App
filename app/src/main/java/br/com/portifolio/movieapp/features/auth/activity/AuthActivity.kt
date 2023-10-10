090f3aa5432b15924360e5425145aad4577bffb4
Anderson Chaves <108032363+chaavez@users.noreply.github.com>
2023-10-09 23:47:12 -0300

    added application authentication navigation, including onboard
    Splash screen removed so far

---
+++ app/src/main/java/br/com/portifolio/movieapp/features/auth/activity/AuthActivity.kt
@@ -0,0 +1,12 @@
+package br.com.portifolio.movieapp.features.auth.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.portifolio.movieapp.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}

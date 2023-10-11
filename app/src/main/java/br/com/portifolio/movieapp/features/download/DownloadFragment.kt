6c0b2a00620e0ba507082767620c7297e7f8797f
Anderson Chaves <108032363+chaavez@users.noreply.github.com>
2023-10-11 13:45:40 -0300

    inflate binding xml in all fragment features

---
+++ app/src/main/java/br/com/portifolio/movieapp/features/download/DownloadFragment.kt
@@ -0,0 +1,27 @@
+package br.com.portifolio.movieapp.features.download
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.com.portifolio.movieapp.databinding.FragmentDownloadBinding
+
+class DownloadFragment : Fragment() {
+
+    private var _binding: FragmentDownloadBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDownloadBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

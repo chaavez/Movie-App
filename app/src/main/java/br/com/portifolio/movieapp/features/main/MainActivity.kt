d1f1d5bb1d6f9950edd67cfcd00a242ce4078591
Anderson Chaves <108032363+chaavez@users.noreply.github.com>
2023-10-11 14:51:44 -0300

    Configured navigation between fragments with the bottombar

--- app/src/main/java/br/com/portifolio/movieapp/features/main/MainActivity.kt
+++ app/src/main/java/br/com/portifolio/movieapp/features/main/MainActivity.kt
@@ -2,7 +2,14 @@ package br.com.portifolio.movieapp.features.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.VISIBLE
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.isVisible
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import br.com.portifolio.movieapp.R
 import br.com.portifolio.movieapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,11 +18,29 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_main_container) as NavHostFragment
+        navController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bottomNavigation, navController)
+
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            binding.bottomNavigation.isVisible =
+                destination.id == R.id.menu_home ||
+                        destination.id == R.id.menu_search ||
+                        destination.id == R.id.menu_favorite ||
+                        destination.id == R.id.menu_download ||
+                        destination.id == R.id.menu_profile
+        }
     }
 }
